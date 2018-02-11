4dda798a22963d338ceef2c48399501f20fcbd80
Vincent Santaiti <vsantaiti@gmail.com>
2018-02-10 22:06:25 -0500

    set debug

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+    }
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+    %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
 @app.route('/<name>')
 def index(name):
     return 'Hello, {}!'.format('name')
