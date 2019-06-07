b91ebf334f89e0fb5b6dbae3f385399c3795350e
Vincent Santaiti <vsantaiti@gmail.com>
2019-06-07 19:15:13 -0400

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -22,5 +22,5 @@ class BaseTestCase(unittest.TestCase):
 class TestIndexCase(BaseTestCase):
 
     def test_index(self):
-        response = self.app.post('/', data={'name': 'Mario'})
+        response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
