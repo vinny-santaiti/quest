3abab635a804c3f09caf40ec75045a2af392aad7
Vincent Santaiti <vsantaiti@gmail.com>
2019-06-07 18:46:00 -0400

    create

---
+++ tests/test_index.py
@@ -0,0 +1,25 @@
+from flask import url_for
+from flask.ext.testing import TestCase
+
+from . import app
+
+
+class BaseTestCase(TestCase):
+    """A base test case for flask app"""
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class TestIndexCase(BaseTestCase):
+
+    def test_index(self):
+        response = self.client.post(url_for('index'), data={'name': 'Mario'})
+        self.assertEqual(response.status_code, 200)
